bed490dab6e65233758325a2c35600e3c45c9ff7
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-04-25 12:41:53 -0500

    added descriptive comments and semi-colons.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,13 @@
-console.log('***** Music Collection *****')
-
+console.log('***** Music Collection *****');
+//initialize array
 let collection = [];
-
+//this function will add albums to the collection--it has inputs for album title, artist name, year published, and an array of tracks
 function addToCollection (titleInput, artistInput, yearPublishedInput, tracksInput) {
     collection.push({title: titleInput, artist: artistInput, yearPublished: yearPublishedInput, tracks:tracksInput});
-
+    console.log(`Just added:`);
+    console.log(collection[collection.length-1]);
 }
-
+//add albums to array
 addToCollection("Ys", "Joanna Newsom", "2006",[{name:"Emily", duration:'12:07'},{name:"Monkey & Bear", duration: "9:29"},{name:	"Sawdust & Diamonds", duration:"9:54"},{name:"Only Skin", duration:"16:53"},{name:"Cosmia", duration:"7:15"}]);
 addToCollection("Helioself", "Papas Fritas", "1997",[{name:"Hey Hey You Say",duration:"3:14"},{name:"We've Got All Night",duration:"3:04"},{name:"Say Goodbye",duration:"4:04"},{name:"Small Rooms",duration:"2:23"},{name:"Rolling in the Sand",duration:"1:48"},{name:"Live by the Water",duration:"2:45"},{name:"Words to Sing",duration:"2:48"},{name:"Sing About Me",duration:"2:28"},{name:"Just to See You",duration:"3:54"},{name:"Captain of the City",duration:"3:50"},{name:"Weight",duration:"1:49"},{name:"Starting to Be It",duration:"2:54"}]);
 addToCollection("Punisher", "Phoebe Bridgers", "2020",[{name:"DVD Menu",duration:"1:09"},{name:"Garden Song",duration:"3:39"},{name:"Kyoto",duration:"3:04"},{name:"Punisher",duration:"3:09"},{name:"Halloween",duration:"4:31"},{name:"Chinese Satellite",duration:"3:37"},{name:"Moon Song",duration:"4:37"},{name:"Savior Complex",duration:"4:01"},{name:"ICU",duration:"3:10"},{name:"Graceland Too",duration:"3:56"},{name:"I Know the End",duration:"5:44"}]);
@@ -16,29 +17,34 @@ addToCollection("Freshman Year", "Hop Along, Queen Ansleis", "2005",[{name:"-",d
 addToCollection("When your Heartstrings Break", "Beulah", "1999",[{name:"Score from Augusta",duration:"2:54"},{name:"Sunday Under Glass",duration:"2:54"},{name:"Matter vs. Space",duration:"3:00"},{name:"Emma Blowgun's Last Stand",duration:"5:21"},{name:"Calm Go the Wild Seas",duration:"3:01"},{name:"Ballad of the Lonely Argonaut",duration:"2:29"},{name:"Comrade's Twenty Sixth",duration:"1:53"},{name:"The Aristocratic Swells",duration:"2:55"},{name:"Silverado Days",duration:"3:39"},{name:"Warmer",duration:"2:56"},{name:"If We Can Land a Man on the Moon, Surely I Can Win Your Heart",duration:"3:17"}]);
 addToCollection("Stranger in the Alps", "Phoebe Bridgers", "2017",[{name:"Smoke Signals",duration:"5:24"},{name:"Motion Sickness",duration:"3:49"},{name:"Funeral",duration:"3:52"},{name:"Demi Moore",duration:"3:18"},{name:"Scott Street",duration:"5:05"},{name:"Killer",duration:"3:09"},{name:"Georgia",duration:"4:07"},{name:"Chelsea",duration:"4:42"},{name:"Would You Rather",duration:"3:19"},{name:"You Missed My Heart",duration:"6:57"},{name:"Smoke Signals (Reprise)",duration:"0:33"}])
 
+//log the collection array
+console.log("This is the collection array:");
 console.log(collection);
 
+//this function will be used to show the collection
 function showCollection (showInput) {
     console.log(`The collection has ${showInput.length} items.`);
-    console.log(`The album(s):`)
+    console.log(`The album(s):`);
     for (let x=0; x<showInput.length; x++) {
         console.log(`${showInput[x].title} by ${showInput[x].artist}, released in ${showInput[x].yearPublished}`);
     }
 }
 
+//test the show collection function
 showCollection(collection);
 
+//this function will find all objects in the collection that have the inputted artist trait
 function findByArtist (stringInput) {
     let artistArray=[];
-    let i=0
+    let i=0;
     for (let x=0; x<collection.length; x++) {
         if(stringInput===collection[x].artist) {
             artistArray.push(collection[x]);
-            i++
+            i++;
         }
     }
     if (i>0) {
-        console.log(artistArray)
+        console.log(artistArray);
         return artistArray;
     }  else {
         return [];
@@ -46,25 +52,18 @@ function findByArtist (stringInput) {
     }
 }
 
+//Test the find by artist function.
 console.log(`What Joanna Newsom albums are in the collection?`);
 showCollection(findByArtist("Joanna Newsom"));
-console.log(`What Julien Baker albums are in the collection?`)
+console.log(`What Julien Baker albums are in the collection?`);
 showCollection(findByArtist("Julien Baker"));
 console.log(`What Phoebe Bridgers albums are in the collection?`);
 showCollection(findByArtist("Phoebe Bridgers"));
 
-//    - Create a function called `search`. This function should:
-//    - Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
-//    ```
-//    { artist: 'Ray Charles', year: 1957 }
-//    ```    
-//  - The returned output from `search` should meet these requirements:
-//- Return a new array of all items in the `collection` matching *all* of the search criteria.
-//- If no results are found, return an empty array.
-//- If there is no search object or an empty search object provided as input, then return all albums in the `collection`.
+//this function will search for any matching albums with a track search overriding other forms of search
 function search (inputObject) {
-    let newArray = []
-    if (inputObject===undefined) {
+    let newArray = [];
+    if (inputObject===undefined) { //check if search was empty
         return collection;
     }
     if (Object.keys(inputObject).includes("track")) { //check if inputObject has a track property
@@ -75,27 +74,27 @@ function search (inputObject) {
                 }
             }
         }
-    } else {
+    } else { //otherwise look at the information provided besides track name
         for (let x of collection) {
-            let i=0;
-            if (inputObject.artist!=undefined) {
+            let i=0; //iterator to count the matching criteria to ensure all parts are found before adding
+            if (inputObject.artist!=undefined) { //will skip if no artist provided
                 if (inputObject.artist===x.artist) {
-                    i++
+                    i++;
                 }
             }
 
-            if (inputObject.album!=undefined) {
+            if (inputObject.album!=undefined) {  //will skip if no album is found
                 if (inputObject.album===x.album) {
-                    i++
+                    i++;
                 }
             }
 
-            if (inputObject.yearPublished!=undefined) {
+            if (inputObject.yearPublished!=undefined) { //will skip if no year published is found
                 if (inputObject.yearPublished===x.yearPublished) {
-                    i++
+                    i++;
                 }
             }
-            if (i===Object.keys(inputObject).length) {
+            if (i===Object.keys(inputObject).length) { //if all non-track inputs have matches in the collection
                 newArray.push(x);
             }
             
@@ -103,34 +102,17 @@ function search (inputObject) {
     }
     return newArray;
 }
-console.log("Search for '1999'")
+
+//Test the search function for various inputs that have one or more possible matches
+console.log("Search for '1999'");
 console.log(search({yearPublished:"1999"}));
-console.log("Search for 'Phoebe Bridgers'")
+console.log("Search for 'Phoebe Bridgers'");
 console.log(search({artist:"Phoebe Bridgers"}));
-console.log("Search for 'Kyoto'")
+console.log("Search for 'Kyoto'");
 console.log(search({track:"Kyoto"}));
-console.log("Search for '2010' and 'Phoebe Bridgers'")
+console.log("Search for '2010' and 'Phoebe Bridgers'");
 console.log(search({artist:"Phoebe Bridgers",yearPublished:"2010"}));
-console.log("Search for '2017' and 'Phoebe Bridgers'")
+console.log("Search for '2017' and 'Phoebe Bridgers'");
 console.log(search({artist:"Phoebe Bridgers",yearPublished:"2017"}));
-console.log("Search for nothing and return full collection")
+console.log("Search for nothing and return full collection");
 console.log(search());
-
-//- Add an array of `tracks` to your album objects. Each track should have a `name` and `duration`. You will need to update the functions to support this new property:
-//- Update the `addToCollection` function to also take an input parameter for the array of tracks.
-//''- Update `search` to allow a `trackName` search criteria. 
-//  - IF the search object has a `trackName` property, only search for that, ignoring any `artist` or `year` properties.
-//- Update the `showCollection` function to display the list of tracks for each album with its name and duration.
-//```
-//  TITLE by ARTIST, published in YEAR:
-//  2. NAME: DURATION
-//  3. NAME: DURATION
-//  TITLE by ARTIST, published in YEAR:
-//  1. NAME: DURATION
-//  2. NAME: DURATION
-//```
-
-
-
-
-
